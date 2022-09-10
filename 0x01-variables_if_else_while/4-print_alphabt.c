fa59a7fd36416f5185d0e0fa6e74e7c6b6c02966
Davewonder Emmanson <davidemmanson001@gmail.com>
2022-09-09 17:58:45 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -  Print the alphabet in lower case
+ * except q and e, followrd by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+
