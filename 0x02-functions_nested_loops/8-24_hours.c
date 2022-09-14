d8d062800f0ed677966e78fdabaf1c78aa32e70c
Davewonder Emmanson <davidemmanson001@gmail.com>
2022-09-13 17:52:19 -0700

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Write a function that prints every minute of the day
+ *
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int H, M;
+
+	for (H = 0; H <= 23; H++)
+	{
+		for (M = 0; M <= 59; M++)
+		{
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
