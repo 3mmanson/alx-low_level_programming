2e3b5d6858de67b8752c0f72d7a8884e499340cb
Davewonder Emmanson <davidemmanson001@gmail.com>
2022-10-02 17:15:56 -0700

    path fix

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * @c: An integer input
+ *
+ * Return: 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @n: An integer input
+ *
+ * Return: the absolute value of n
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
